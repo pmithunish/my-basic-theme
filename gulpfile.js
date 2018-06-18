004cfa3db10b2868dcf2e05800b665d77fe26840
Mithunish Prabhakaran <mithun@animapp.in>
2018-06-19 04:01:50 +0530

    improved the gulpfile and added create new project if not already present and install material packages and also made everything in sequential flow

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const options = require('gulp-options');
 const inject = require('gulp-inject-string');
+const exec = require('child_process').exec;
+const fs = require('fs');
 
 const name = options.get('name') || undefined;
 
@@ -27,9 +29,38 @@ const paths = {
   }
 };
 
-gulp.task('inject-material-icons', () => {
-  gulp
+gulp.task('create-project', cb => {
+  if (!fs.existsSync(name)) {
+    exec('ng new ' + name + ' --style=scss', (err, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
+  } else {
+    cb(new Error('project already found'));
+  }
+});
+
+gulp.task('install-material', ['create-project'], cb => {
+  exec(
+    'cd ' + name + ' && npm install --save @angular/material @angular/cdk',
+    (err, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    }
+  );
+});
+
+gulp.task('inject-material-icons', ['install-material'], () => {
+  const stream = gulp
     .src(paths.INJECT_MATERIAL_ICONS.SRC)
+    .pipe(
+      inject.replace(
+        '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">',
+        ''
+      )
+    )
     .pipe(
       inject.after(
         '<link rel="icon" type="image/x-icon" href="favicon.ico">',
@@ -38,35 +69,37 @@ gulp.task('inject-material-icons', () => {
     )
     .pipe(inject.replace('<body>', '<body class="mat-typography">'))
     .pipe(gulp.dest(paths.INJECT_MATERIAL_ICONS.DEST));
+  return stream;
 });
 
-gulp.task('inject-my-theme', () => {
-  gulp
+gulp.task('inject-my-theme', ['inject-material-icons'], () => {
+  const stream = gulp
     .src(paths.INJECT_MY_THEME.SRC)
+    .pipe(inject.replace('"src/my-theme.scss", ', ''))
     .pipe(inject.afterEach('"styles": [', '"src/my-theme.scss", '))
     .pipe(gulp.dest(paths.INJECT_MY_THEME.DEST));
+  return stream;
 });
 
-gulp.task('copy-my-theme', () => {
-  gulp.src(paths.COPY_MY_THEME.SRC).pipe(gulp.dest(paths.COPY_MY_THEME.DEST));
+gulp.task('copy-my-theme', ['inject-my-theme'], () => {
+  const stream = gulp
+    .src(paths.COPY_MY_THEME.SRC)
+    .pipe(gulp.dest(paths.COPY_MY_THEME.DEST));
+  return stream;
 });
 
-gulp.task('copy-stylesheets', () => {
-  gulp
+gulp.task('copy-stylesheets', ['copy-my-theme'], () => {
+  const stream = gulp
     .src(paths.COPY_STYLESHEETS.SRC)
     .pipe(gulp.dest(paths.COPY_STYLESHEETS.DEST));
+  return stream;
 });
 
-gulp.task('copy-main-styles', () => {
-  gulp
+gulp.task('copy-main-styles', ['copy-stylesheets'], () => {
+  const stream = gulp
     .src(paths.COPY_MAIN_STYLES.SRC)
     .pipe(gulp.dest(paths.COPY_MAIN_STYLES.DEST));
+  return stream;
 });
 
-gulp.task('default', [
-  'inject-material-icons',
-  'inject-my-theme',
-  'copy-my-theme',
-  'copy-main-styles',
-  'copy-stylesheets'
-]);
+gulp.task('default', ['copy-main-styles']);
